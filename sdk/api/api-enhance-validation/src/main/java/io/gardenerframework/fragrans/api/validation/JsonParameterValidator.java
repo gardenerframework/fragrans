907a22563310db751210445f872c1448bdae0083
张涵 <zhanghan30@jd.co>
2023-04-01 00:45:49 +0800

    chore(api-enhance-validation): JsonParameterValidator增加几个方法

--- sdk/api/api-enhance-validation/src/main/java/io/gardenerframework/fragrans/api/validation/JsonParameterValidator.java
+++ sdk/api/api-enhance-validation/src/main/java/io/gardenerframework/fragrans/api/validation/JsonParameterValidator.java
@@ -3,12 +3,15 @@ package io.gardenerframework.fragrans.api.validation;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.gardenerframework.fragrans.api.standard.error.exception.client.BadRequestArgumentException;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.lang.Nullable;
 import org.springframework.util.CollectionUtils;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.Path;
 import javax.validation.Validator;
+import javax.validation.constraints.NotNull;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -17,24 +20,58 @@ import java.util.Set;
  * 用来验证那种map格式的数据
  */
 @RequiredArgsConstructor
+@SuppressWarnings("rawtypes")
 public class JsonParameterValidator {
     private final ObjectMapper objectMapper = new ObjectMapper();
     private final Validator validator;
 
-    @SuppressWarnings("rawtypes")
-    public <T> void validate(Map json, Class<T> targetTpe) {
+    /**
+     * 完成验证
+     *
+     * @param json      json
+     * @param targetTpe 目标类型
+     * @return 是否包含验证失败
+     */
+    @Nullable
+    private <T> Map<String, Object> doValidate(@NonNull Map json, @NotNull Class<T> targetTpe) {
         T t = objectMapper.convertValue(json, targetTpe);
         Set<ConstraintViolation<Object>> violations = validator.validate(t);
+        Map<String, Object> details;
         if (!CollectionUtils.isEmpty(violations)) {
-            Map<String, Object> details = new HashMap<>(violations.size());
+            details = new HashMap<>(violations.size());
             violations.forEach(
                     constraintViolation -> {
                         Path propertyPath = constraintViolation.getPropertyPath();
                         details.put(propertyPath.toString(), constraintViolation.getMessage());
                     }
             );
-            throw new BadRequestArgumentException(details);
+            return details;
         }
+        return null;
+    }
 
+    /**
+     * 尝试验证，不抛异常
+     *
+     * @param json      json
+     * @param targetTpe 目标类型
+     * @return 验证是否成功
+     */
+    public <T> boolean tryValidate(@NonNull Map json, @NotNull Class<T> targetTpe) {
+        return doValidate(json, targetTpe) == null;
+    }
+
+    /**
+     * 验证，失败了就抛异常
+     *
+     * @param json      json
+     * @param targetTpe 目标类型
+     * @throws BadRequestArgumentException 验证失败
+     */
+    public <T> void validate(@NonNull Map json, @NotNull Class<T> targetTpe) throws BadRequestArgumentException {
+        Map<String, Object> violations = doValidate(json, targetTpe);
+        if (violations != null) {
+            throw new BadRequestArgumentException(violations);
+        }
     }
 }
