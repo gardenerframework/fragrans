66e6875f3ff1d4dfe39b994ceb3182a0c520c989
张涵 <zhanghan30@jd.co>
2023-03-31 23:38:04 +0800

    feat(api-standard-error): BadRequestArgumentException支持输入一个Map作为详情

--- sdk/api/api-standard/api-standard-error-exceptions/src/main/java/io/gardenerframework/fragrans/api/standard/error/exception/client/BadRequestArgumentException.java
+++ sdk/api/api-standard/api-standard-error-exceptions/src/main/java/io/gardenerframework/fragrans/api/standard/error/exception/client/BadRequestArgumentException.java
@@ -1,12 +1,19 @@
 package io.gardenerframework.fragrans.api.standard.error.exception.client;
 
+import io.gardenerframework.fragrans.api.standard.error.exception.ApiErrorDetailsSupplier;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 请求参数有问题
  *
  * @author ZhangHan
  * @date 2021/8/20 1:06
  */
-public class BadRequestArgumentException extends BadRequestException {
+public class BadRequestArgumentException extends BadRequestException implements ApiErrorDetailsSupplier {
+    private Map<String, Object> details;
+
     public BadRequestArgumentException() {
     }
 
@@ -24,5 +31,34 @@ public class BadRequestArgumentException extends BadRequestException {
 
     public BadRequestArgumentException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public BadRequestArgumentException(Map<String, Object> details) {
+        this.details = details;
+    }
+
+    public BadRequestArgumentException(String message, Map<String, Object> details) {
+        super(message);
+        this.details = details;
+    }
+
+    public BadRequestArgumentException(String message, Throwable cause, Map<String, Object> details) {
+        super(message, cause);
+        this.details = details;
+    }
+
+    public BadRequestArgumentException(Throwable cause, Map<String, Object> details) {
+        super(cause);
+        this.details = details;
+    }
+
+    public BadRequestArgumentException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace, Map<String, Object> details) {
+        super(message, cause, enableSuppression, writableStackTrace);
+        this.details = details;
+    }
+
+    @Override
+    public Map<String, Object> getDetails() {
+        return details == null ? new HashMap<>() : details;
     }
 }
